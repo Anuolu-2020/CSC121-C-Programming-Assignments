0f8c46dd679f760e35d941e0e002e10c4f88650e
Anuolu-2020 <Anuolu1645@gmail.com>
2024-05-25 00:00:59 +0100

    feat: dangling else question answered

---
+++ dangling-else.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//How to program C Excercise 3.31
+int main(void) {
+  int x = 5;
+  int y = 8;
+
+  if (y == 8) {
+    if (x == 5) {
+      puts("@@@@@");
+    } else {
+      puts("#####");
+      puts("$$$$$");
+      puts("&&&&&");
+    }
+  }
+
+  // Answer is B. @@@@@
+}
