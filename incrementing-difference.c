064c1c65cbbe89cd84cad10f56bf6df51e5192fd
Anuolu-2020 <Anuolu1645@gmail.com>
2024-05-24 23:38:59 +0100

    feat: pre increment and post increment implemented

---
+++ incrementing-difference.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int variable = 3;
+
+  printf("Difference between pre incrementing and post incrementing.\n");
+  printf("\n");
+  printf("Initial value of variable = %i\n", variable);
+  printf("Using pre incrementing (++variable): %i\n", ++variable);
+  printf("Value of variable after pre-increment: %i\n", variable);
+
+  int variable2 = 3;
+  // printf("%i\n", variable);
+  printf("\n");
+  printf("Initial value of variable2 = %i\n", variable2);
+  printf("Using post incrementing (variable2++): %i\n", variable2++);
+  printf("Value of variable2 after post-increment: %i\n", variable2);
+}
